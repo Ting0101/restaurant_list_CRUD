1f63d23cc3fa5f7be08009b2dc04d696eeb36d39
Ting0101 <microdaisy@gmail.com>
2023-05-13 15:49:00 +0800

    feat: creat a new restaurant, read restaurant information

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser') //載入body-parser
 // const restaurantList = require('./restaurant.json')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -26,6 +27,8 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+//規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 //瀏覽全部餐廳
@@ -33,17 +36,29 @@ app.get('/', (req, res) => {
   restaurant.find()
     .lean()
     .then(restaurantData => res.render('index', { restaurantData }))
-    .catch(error => console.log('error!'))
+    .catch(error => console.log('瀏覽error!'))
+})
+
+//新增一家餐廳-開啟new頁面輸入表單資料
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+//用來接住表單資料的路由
+app.post('/restaurants', (req, res) => {
+  console.log('req.body', req.body)
+  restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log('新增error!'))
 })
 
 
-//瀏覽餐廳資訊
-app.get('/restaurants/:id', (req, res) => {
-  // console.log(req.params.id)
-  const restaurant = restaurantList.results.find(
-    restaurant => restaurant.id.toString() === req.params.id
-  )
-  res.render('show', { restaurant })
+//瀏覽一家餐廳資訊
+app.get(`/restaurants/:id`, (req, res) => {
+  const id = req.params.id
+  restaurant.findById(id)
+    .lean()
+    .then(restaurantData => (res.render('show', { restaurantData })))
+    .catch(error => console.log('餐廳資訊error!'))
 })
 
 app.get('/search', (req, res) => {
